bf6a4517b3a30065e30587426313499d1e6eae5b
thisiscpta <121990524+thisiscpta@users.noreply.github.com>
2023-03-08 11:03:36 +0800

    Update update-jira.sh

--- ansible/update-jira.sh
+++ ansible/update-jira.sh
@@ -2,7 +2,7 @@ echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo $1
 echo $2
 curl -v \
-  --user thisiscpta@gmail.com:ATATT3xFfGF03SLJrSLI68PdQzgI4Nbyjh2GlK34z2b7-o5Oaa2OO3BZ295_AelpK_q73XpKAyDYd8h55V0J4ORxfXNRQuZLZqv4AYa_MizXAOJCLRuU5LD0_y0eGcEwmZoPVUWx5dp76RbqjRqwkHvaPx9I8L35R9856clBMJXhQjrjr6L5bJk=032824BD \
+  --user $3 \
   --header 'Accept: application/json' \
   --header 'Content-Type: application/json' \
   --request PUT \
